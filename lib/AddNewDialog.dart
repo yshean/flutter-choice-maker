1762623146def8a07ff590ff06144fa5b3728079
Yong Shean CHONG <yong.shean.chong@comerge.net>
2019-10-04 13:58:20 +0800

    Building Add New screen

--- lib/AddNewDialog.dart
+++ lib/AddNewDialog.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'models/Choice.dart';
 
-class AddNewDialog extends StatelessWidget {
+class AddNewDialog extends StatefulWidget {
   const AddNewDialog({Key key});
+
+  @override
+  _AddNewDialogState createState() => _AddNewDialogState();
+}
+
+class _AddNewDialogState extends State<AddNewDialog> {
+  double _sliderValue = 5.0;
+  static List<DropdownMenuItem> _dropdownMenuItems = [
+    DropdownMenuItem(value: "What for lunch?", child: Text("What for lunch?")),
+    DropdownMenuItem(
+        value: "What for dinner?", child: Text("What for dinner?")),
+  ];
+  String _selectedCategory = _dropdownMenuItems[0].value;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +37,61 @@ class AddNewDialog extends StatelessWidget {
           ),
         ],
       ),
-      body: Text("Form here"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                  icon: Icon(Icons.description), labelText: "Answer"),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Please enter your answer";
+                }
+                return null;
+              },
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 14.0),
+                    child: Icon(Icons.favorite),
+                  ),
+                  Text("0"),
+                  Expanded(
+                    child: Slider(
+                      label: "Likelihood",
+                      min: 0.0,
+                      max: 10.0,
+                      divisions: 10,
+                      onChanged: (value) {
+                        setState(() => _sliderValue = value);
+                      },
+                      value: _sliderValue,
+                    ),
+                  ),
+                  Text("10"),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: DropdownButton(
+                value: _selectedCategory,
+                items: _dropdownMenuItems,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
